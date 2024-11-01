d07834e4726422606d8d00bbf6e2a637372c0e87
Aviyas-dev <aviyas.amgalanbaatar@gmail.com>
2024-11-01 18:27:51 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// // Change heading:
+// document.getElementById("myH").innerHTML = "Comments can make code readable";
+/* Comments can make code readable */
+//"Javascript lesson begin"
+/*Comments c
+an make code 
+readable*/
---
+++ variable.js
@@ -0,0 +1,7 @@
+let number = 1;
+let text = "Hello";
+text = (true);
+let x;
+text = null;
+
+console.log(number, text, x)
