a5e011930c726b38e71104425c23e05d3ccd236a
Aviyas-dev <aviyas.amgalanbaatar@gmail.com>
2024-11-04 14:35:37 +0800

    bodlogo

---
+++ while.js
@@ -0,0 +1,39 @@
+// let i = 0;
+
+// while( i<37){
+//     if(i%5===0){
+//   console.log(21,i);}
+//   i++;
+// };
+
+
+
+// let a= 5, b= 3, c= 8, d= 6;
+// let multiple= 1;
+
+// if (a % 2 == 0){
+//     multiple *= a
+// }
+//     if(b % 2 == 0){multiple *= b
+//    }
+//     if(c % 2 == 0){multiple *= c}
+//     if(d % 2 == 0){multiple *= d}
+//     console.log(23,multiple)
+
+
+
+ let n= 7, a = 0;
+ while(n != 1){ 
+     if(n % 2 == 0){
+         n = n/2;
+         a++;
+        
+    
+ }else {
+    
+
+   n= ((n*3)+1 )
+     a++;
+ }
+ }
+ console.log(a)
